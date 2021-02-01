42f3c542b542a1695e882b0871bfc481b2930240
abhi6676 <iamlive247@gmail.com>
2021-02-02 04:21:06 +0530

    before adding useLoginHook

---
+++ dist/src/components/common/useLoginHook/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import { Text, View } from "react-native"
+
+
+
+
+
+interface Props {
+
+
+
+}
+
+
+
+export const index = (props: Props) => {
+
+
+
+
+
+    const Component = ({ children }: any) => {
+
+        return (
+
+            <View>
+
+                <Text>text</Text>
+
+                {children}
+
+            </View>
+
+        )
+
+    }
+
+
+
+
+
+
+
+    return [Component]
+
+}
+
---
+++ dist/src/components/common/useLoginHook/readme.md
@@ -0,0 +1,6 @@
+# useLoginHook
+
+
+
+returns an element node that consumes a react children to render and provides login helper function
+
