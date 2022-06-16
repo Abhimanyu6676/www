318dc005ceb583073a25cc15e0affc16e42342c4
abhimanyu6676 <iamlive24@gmail.com>
2022-06-16 21:54:20 +0530

    - contact us page UI improvements
    - contact links setup
    
    - global.css `a` tag color set to `unset`

--- src/components/common/contactUs.tsx
+++ src/components/common/contactUs.tsx
@@ -27,22 +27,44 @@ export const ContactUs = (props: Props) => {
         }}
       >
         <h1 style={{ textAlign: "center" }}>Connect with us</h1>
-        <p style={{ textAlign: "center" }}>
+        <p style={{ textAlign: "center", marginTop: 20 }}>
           Leave your enquiry or request. On sunny days we reply within 30 mins.
           or if it's about troubleshoot than call us right away
         </p>
         <div style={{ marginTop: 50 }}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <AlternateEmailIcon fontSize="medium" />
-            <p style={{ marginLeft: 10 }}>suport@huelite.in</p>
+          <div>
+            <a href="mailto:support@huelite.in?subject=I%20need%20assistance%20with%20my%20HUElite%20product.">
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <AlternateEmailIcon fontSize="medium" />
+                <p style={{ marginLeft: 10 }}>suport@huelite.in</p>
+              </div>
+            </a>
           </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <CallIcon fontSize="medium" />
-            <p style={{ marginLeft: 10 }}>+91 9582463222</p>
+
+          <div
+            style={{
+              marginTop: 20,
+            }}
+          >
+            <a href="tel:+919871532573">
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <CallIcon fontSize="medium" />
+                <p style={{ marginLeft: 10 }}>+91 9871532573</p>
+              </div>
+            </a>
           </div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <WhatsAppIcon fontSize="medium" />
-            <p style={{ marginLeft: 10 }}>Tap to connect on whatsapp</p>
+
+          <div
+            style={{
+              marginTop: 20,
+            }}
+          >
+            <a href="https://wa.me/+919871532573?text=Hi,%20I%20need%20support">
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <WhatsAppIcon fontSize="medium" />
+                <p style={{ marginLeft: 10 }}>Tap to connect on whatsapp</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
